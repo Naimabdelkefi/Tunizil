140faba8cf028c1add8fdaec5da2380ff87abfb5
root <you@.com>
2017-08-14 20:54:14 +0100

    Festivals page

--- wp-content/themes/sydney/footer.php
+++ wp-content/themes/sydney/footer.php
@@ -18,11 +18,7 @@
     <a class="go-top"><i class="fa fa-angle-up"></i></a>
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info container">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'sydney' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'sydney' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %2$s by %1$s.', 'sydney' ), 'aThemes', '<a href="http://athemes.com/theme/sydney" rel="designer">Sydney</a>' ); ?>
-		</div><!-- .site-info -->
+		
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
